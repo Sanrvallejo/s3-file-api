80084ac776b0c80cedaa2c8bc25eba879397b5b8
Sanrvallejo <zanti-23@hotmail.com>
2025-06-01 22:15:32 -0500

    metodo: lsitar objetos en del bucket

--- src/main/java/com/webdev/awsS3/services/FileService.java
+++ src/main/java/com/webdev/awsS3/services/FileService.java
@@ -1,4 +1,35 @@
 package com.webdev.awsS3.services;
 
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class FileService {
+
+    private final S3Client s3Client;
+
+    public FileService(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public List<String> listFilesInFolder(String bucketName, String folderPrefix) {
+        String normalizedPrefix = folderPrefix.endsWith("/") ? folderPrefix : folderPrefix + "/";
+
+        ListObjectsV2Request request = ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .prefix(normalizedPrefix)
+                .build();
+
+        ListObjectsV2Response response = s3Client.listObjectsV2(request);
+
+        return response.contents().stream()
+                .map(S3Object::key)
+                .collect(Collectors.toList());
+    }
 }
