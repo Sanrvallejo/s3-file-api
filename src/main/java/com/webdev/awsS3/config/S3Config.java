efb50656919248d2b0dd7bd86568e7a3ab021112
Sanrvallejo <zanti-23@hotmail.com>
2025-06-01 22:14:35 -0500

    clase: configuracion para conectar con aws

--- src/main/java/com/webdev/awsS3/config/S3Config.java
+++ src/main/java/com/webdev/awsS3/config/S3Config.java
@@ -1,4 +1,29 @@
 package com.webdev.awsS3.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
 public class S3Config {
+
+    @Bean
+    public S3Client s3Client() {
+        Dotenv dotenv = Dotenv.load();
+
+        String accessKey = dotenv.get("AWS_ACCESS_KEY");
+        String secretKey = dotenv.get("AWS_SECRET_KEY");
+        String region = dotenv.get("AWS_REGION");
+
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
 }
