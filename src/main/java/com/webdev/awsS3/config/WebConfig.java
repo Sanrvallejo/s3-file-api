5af7c6d76e5548efb19bc8fcc212c033e56510e4
Sanrvallejo <zanti-23@hotmail.com>
2025-06-01 22:55:48 -0500

    habilitacion de CORS

---
+++ src/main/java/com/webdev/awsS3/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.webdev.awsS3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // permite a todos los endpoints
+                        .allowedOrigins("http://127.0.0.1:5500") // o el puerto de tu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
