ad1bc1f2d7cc1d6e8e661ba2c78e142b3dcbef77
Sanrvallejo <zanti-23@hotmail.com>
2025-06-01 22:16:00 -0500

    endpoint: lsitar objetos en del bucket

--- src/main/java/com/webdev/awsS3/controllers/FileController.java
+++ src/main/java/com/webdev/awsS3/controllers/FileController.java
@@ -1,4 +1,25 @@
 package com.webdev.awsS3.controllers;
 
+import com.webdev.awsS3.services.FileService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/files")
 public class FileController {
+
+    private final FileService fileService;
+
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @GetMapping("/list")
+    public List<String> listFiles(
+            @RequestParam String bucket,
+            @RequestParam String folder
+    ) {
+        return fileService.listFilesInFolder("my-api-s3-files","images");
+    }
 }
+
